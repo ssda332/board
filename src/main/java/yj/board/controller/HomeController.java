e3041219358ed6a2b979453e2d30e09f9416d106
ssda332 <tititiqkfk3@gmail.com>
2023-08-11 16:26:57 +0900

    test: 브랜치 변경 테스트

--- src/main/java/yj/board/controller/HomeController.java
+++ src/main/java/yj/board/controller/HomeController.java
@@ -11,14 +11,15 @@ public class HomeController {
     @GetMapping("/")
     public String homeLoginModel(@Login Member loginMember, Model model) {
 
-        // 세션에 회원 데이터가 없으면 members/login
+        /*// 세션에 회원 데이터가 없으면 members/login
         if (loginMember == null) {
             return "members/login";
         }
 
         // 세션이 유지되면 로그인으로 이동
         model.addAttribute("member", loginMember);
-        return "home";
+        return "home";*/
+        return "index";
     }
 
 //    @GetMapping("/")
