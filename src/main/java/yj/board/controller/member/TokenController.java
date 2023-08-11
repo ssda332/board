6db18b8178e59be2ff95b733aeca6399cbbe32e9
ssda332 <tititiqkfk3@gmail.com>
2023-08-11 16:48:43 +0900

    test: 브랜치 변경 테스트4

--- src/main/java/yj/board/controller/member/TokenController.java
+++ src/main/java/yj/board/controller/member/TokenController.java
@@ -8,19 +8,18 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import yj.board.domain.dto.LoginDto;
 import yj.board.domain.dto.TokenDto;
+import yj.board.domain.member.Member;
 import yj.board.jwt.JwtFilter;
 import yj.board.jwt.TokenProvider;
 
 import javax.validation.Valid;
 
 @Slf4j
-@RestController
+@Controller
 @RequestMapping("/token")
 public class TokenController {
     private final TokenProvider tokenProvider;
@@ -31,7 +30,42 @@ public class TokenController {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
     }
 
+    @GetMapping("/new")
+    public String loginForm(@ModelAttribute("member") Member member) {
+        return "members/login";
+    }
+
+    @ResponseBody
     @PostMapping("")
+    public ResponseEntity<TokenDto> login(@Valid @RequestBody LoginDto loginDto) {
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getLoginId(), loginDto.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+//        log.debug("authentication : {}", authentication);
+        String jwt = tokenProvider.createToken(authentication);
+//        log.debug("jwt : {}", jwt);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+        /*memberService.login(loginDto);
+
+        토큰을 쿠키로 발급 및 응답에 추가
+        Cookie cookie = new Cookie(JwtFilter.AUTHORIZATION_HEADER, jwt);
+        cookie.setMaxAge(7 * 24 * 60 * 60); // 7일 동안 유효
+        cookie.setPath("/");
+        cookie.setDomain("localhost");
+        cookie.setSecure(false);
+
+        response.addCookie(cookie);
+        ResponseEntity res = new ResponseEntity();*/
+
+        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
+    }
+
+    /*@PostMapping("")
     public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto loginDto) {
 
         log.debug("/authenticate start");
@@ -51,5 +85,5 @@ public class TokenController {
         httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
 
         return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
-    }
+    }*/
 }
