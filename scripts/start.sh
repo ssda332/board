8b672a64895e5c58500c6e75c853da97eaa5e109
ssda332 <tititiqkfk3@gmail.com>
2024-03-04 19:52:57 +0900

    chore: start.sh 수정
    prod 환경 실행 스크립트문 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,7 +13,7 @@ echo "[$NOW] $JAR 복사" >> $START_LOG
 cp $ROOT_PATH/build/libs/board-project-1.0.0.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar -Dspring.profiles.active=prod $JAR > $APP_LOG 2> $ERROR_LOG &
 
 SERVICE_PID=$(pgrep -f $JAR)
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
