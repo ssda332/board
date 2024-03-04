136cd8325cf1d97500d9f8e061f1f85a863200ab
ssda332 <tititiqkfk3@gmail.com>
2024-03-04 22:21:46 +0900

    chore: cicd test 2

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,8 +13,11 @@ NOW=$(date +%c)
 echo "[$NOW] $JAR 복사" >> $START_LOG
 cp $ROOT_PATH/build/libs/board-0.0.1-SNAPSHOT.jar $JAR
 
+echo "[$NOW] ssl 복사" >> $START_LOG
+ cp -R $KEYSTORE_PATH $KEYSTORE_CP_PATH
+
 echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar -Dspring.profiles.active=prod $JAR > $APP_LOG 2> $ERROR_LOG &
 
 SERVICE_PID=$(pgrep -f $JAR)
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
