8945950148eb408f0c68bc58935eb732a3253d33
ssda332 <tititiqkfk3@gmail.com>
2024-03-04 19:59:09 +0900

    chore: 배포 스크립트 수정
    root 경로 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOT_PATH="/home/ec2-user/board-project"
+ROOT_PATH="/home/ec2-user"
 JAR="$ROOT_PATH/application.jar"
 
 APP_LOG="$ROOT_PATH/application.log"
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOT_PATH="/home/ec2-user/board-project"
+ROOT_PATH="/home/ec2-user"
 JAR="$ROOT_PATH/application.jar"
 STOP_LOG="$ROOT_PATH/stop.log"
 SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
